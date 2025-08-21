8956e085b07bf9b180c2d125af2a332776448659
Vinicius Martin <viniciusmartindeoliveira@hotmail.com>
2025-08-21 23:17:23 +0000

    feat: Adicionando o cadastro dos territórios

--- war.c
+++ war.c
@@ -102,3 +102,17 @@ int main() {
 
     printf("--- Desafio WAR: Nivel Aventureiro ---\n\n");
 
+     for (int i = 0; i < num_territorios; i++) {
+        printf("Territorio %d:\n", i + 1);
+        printf("  Nome: ");
+        fgets(mapa[i].nome, sizeof(mapa[i].nome), stdin);
+        mapa[i].nome[strcspn(mapa[i].nome, "\n")] = '\0';
+        printf("  Cor do exercito: ");
+        fgets(mapa[i].cor_exercito, sizeof(mapa[i].cor_exercito), stdin);
+        mapa[i].cor_exercito[strcspn(mapa[i].cor_exercito, "\n")] = '\0';
+        printf("  Numero de tropas: ");
+        scanf("%d", &mapa[i].num_tropas);
+        while (getchar() != '\n');
+    }
+}
+
