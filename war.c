6277a7c3bf65c9b3939535edbaa426c676b67338
Vinicius Martin <viniciusmartindeoliveira@hotmail.com>
2025-08-21 22:46:26 +0000

    feat: definindo as classes correspondentes

--- war.c
+++ war.c
@@ -15,4 +15,17 @@ int main() {
     printf("--- Desafio WAR: Nivel Novato ---\n\n");
 
     for (i = 0; i < 5; i++) {
-        printf("Territorio %d:\n", i + 1);}
+        printf("Territorio %d:\n", i + 1);
+        
+           printf("  Nome: ");
+        fgets(mapa[i].nome, sizeof(mapa[i].nome), stdin);
+        mapa[i].nome[strcspn(mapa[i].nome, "\n")] = '\0';
+
+        printf("  Cor do exercito: ");
+        fgets(mapa[i].cor_exercito, sizeof(mapa[i].cor_exercito), stdin);
+        mapa[i].cor_exercito[strcspn(mapa[i].cor_exercito, "\n")] = '\0';
+
+        printf("  Numero de tropas: ");
+        scanf("%d", &mapa[i].num_tropas);
+        while (getchar() != '\n');
+    }
